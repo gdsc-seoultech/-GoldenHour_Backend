11b892bbc3c3e5aabec1bbdd972aa73b4136dcbf
yuseogi0218 <yuseogi0218@gmail.com>
2023-02-11 21:33:10 +0900

    FIX - /user GetMapping -> PostMapping

--- src/main/java/com/gdsc/goldenhour/user/controller/UserController.java
+++ src/main/java/com/gdsc/goldenhour/user/controller/UserController.java
@@ -3,7 +3,7 @@ package com.gdsc.goldenhour.user.controller;
 import com.gdsc.goldenhour.common.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("")
+    @PostMapping("")
     public ResponseEntity<ResponseDto<String>> login() {
         return new ResponseEntity<>(ResponseDto.success("로그인 완료"), HttpStatus.OK);
     }
