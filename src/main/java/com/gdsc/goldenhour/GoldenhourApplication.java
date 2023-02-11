217519337c8891bd37cefe1698f7bc552de7dfd5
yuseogi0218 <yuseogi0218@gmail.com>
2023-02-11 18:21:59 +0900

    init project

---
+++ src/main/java/com/gdsc/goldenhour/GoldenhourApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.goldenhour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenhourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenhourApplication.class, args);
+	}
+
+}
